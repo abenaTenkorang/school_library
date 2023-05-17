540cf9b203ccaecd89a98b245b939011a151c8a3
abenaTenkorang <abenagy16@gmail.com>
2023-05-17 22:07:31 +0000

    Fix linter errors

--- Classes/rental.rb
+++ Classes/rental.rb
@@ -1,11 +1,11 @@
 class Rental
-    attr_accessor :date, :person, :book
-  
-    def initialize(date, person, book)
-      @date = date
-      @person = person
-      @book = book
-      @person.rentals << self unless book.rentals.include?(self)
-      @book.rentals << self unless person.rentals.include?(self)
-    end
+  attr_accessor :date, :person, :book
+
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    @book = book
+    @person.rentals << self unless book.rentals.include?(self)
+    @book.rentals << self unless person.rentals.include?(self)
   end
+end
--- Classes/student.rb
+++ Classes/student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 require_relative 'classroom'
 
 class Student < Person
-  attr_reader  :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
@@ -17,5 +17,4 @@ class Student < Person
   def play_hooky
     '¯\\_(ツ)_/¯'
   end
-
 end
